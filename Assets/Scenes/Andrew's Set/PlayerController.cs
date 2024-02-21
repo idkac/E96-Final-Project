b69b94b2ff1df6ffd95dc66c4f8e9da0ecc606f8
idkac <andrewsun71@gmail.com>
2024-02-21 11:37:08 -0800

    fixed bug with wall slide and jumping near wall and also new collision system (not yet implemented)

--- Assets/Scenes/Andrew's Set/PlayerController.cs
+++ Assets/Scenes/Andrew's Set/PlayerController.cs
@@ -13,6 +13,8 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField] float jumpVelocity = 3f;
     [SerializeField] float slideSpeed = 0.2f;
     [SerializeField] int maxJumpCount = 2;
+
+    public bool onWallRight, onWallLeft, onGround;
     private int allowedJumpCount;
     // Start is called before the first frame update
     void Start()
@@ -33,9 +35,7 @@ public class NewBehaviourScript : MonoBehaviour
         rb.velocity = new Vector2(direction.x * speed, rb.velocity.y);
 
         if(collision.onWall && !collision.onGround)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, -slideSpeed);
-        }
+            wallSlide();
 
         if(collision.onGround)
             allowedJumpCount = maxJumpCount;
@@ -47,11 +47,42 @@ public class NewBehaviourScript : MonoBehaviour
         allowedJumpCount--;
         if (allowedJumpCount > 0)
         {
+        if (collision.onGround)
+        {
         rb.velocity = Vector2.up * jumpVelocity;
         if (rb.velocity.y < 0)
             rb.velocity += Vector2.up * Physics2D.gravity.y * (fall - 1) * Time.deltaTime;
         else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
             rb.velocity += Vector2.up * Physics2D.gravity.y * (jump - 1) * Time.deltaTime;
+        }
+        // else if (collision.onWall)
+        // {
+            //implement wall jump function
+        // }
+        }
+    }
+
+    void wallSlide()
+    {
+        if (rb.velocity.y >= 0)
+            return;
+        if(collision.onWall && !collision.onGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -slideSpeed);
+        }
+    }
+
+//other way to get collision (to be implemented)
+    void OnCollisionEnter2D(Collision2D tempcollision)
+    {   
+        foreach (ContactPoint2D contact in tempcollision.contacts)
+        {
+            if (Vector2.Dot(Vector2.up, contact.normal) >= 0.9)
+                onGround = true;
+            else if (Vector2.Dot(Vector2.left, contact.normal) >= 0.9)
+                onWallRight = true;
+            else if (Vector2.Dot(Vector2.right, contact.normal) >= 0.9)
+                onWallLeft = true;
         }
     }
 }
