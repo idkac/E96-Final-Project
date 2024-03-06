06bd5995532a84d3213d316c9e0bee27ec5676ec
Drewal-Drew <drewal@g.ucla.edu>
2024-03-05 23:12:01 -0800

    Change in Sample Scene

--- Assets/Scenes/Andy's Set/Anim_Script.cs
+++ Assets/Scenes/Andy's Set/Anim_Script.cs
@@ -21,8 +21,10 @@ public class Anim_Script : MonoBehaviour
 
     private Vector2 inputVector;
     private bool facing_left;
-    private bool on_Ground;
+    private bool on_Ground = true;
     private float weapon_use;
+
+
     enum Inventory { Knife, Sword, Gun, Handgun, Shotgun };
     Inventory equipped = 0;
     int number = 1;
@@ -40,7 +42,7 @@ public class Anim_Script : MonoBehaviour
     void Update()
     {
         sp.flipX = facing_left;
-        anim.SetBool("isWalking", Mathf.Abs(rb.velocity.x) > 0.5f);
+        anim.SetBool("isWalking", Mathf.Abs(rb.velocity.x) > 0.5f && on_Ground == true);
 
         //Debug.Log(rb.velocity);
 
@@ -72,45 +74,79 @@ public class Anim_Script : MonoBehaviour
         
 
 
-        Debug.Log(inputVector);
-        Debug.Log(facing_left);
+        //Debug.Log(inputVector);
+       // Debug.Log(facing_left);
     }
 
     void idle()
     {
 
-        Debug.Log("number" + number);
+        //Debug.Log("number" + number);
         switch (number)
         {
             case 1:
                 equipped = Inventory.Knife;
                 anim.SetInteger("Idle_State", number);
-                Debug.Log("Knife");
+                //Debug.Log(equipped);
                 break;
             case 2:
                 equipped = Inventory.Sword;
                 anim.SetInteger("Idle_State", number);
-                Debug.Log("Sword");
+                //Debug.Log(equipped);
                 break;
             case 3:
                 equipped = Inventory.Handgun;
                 anim.SetInteger("Idle_State", number);
-                Debug.Log("Handgun");
+                //Debug.Log(equipped);
                 break;
             case 4:
                 equipped = Inventory.Shotgun;
                 anim.SetInteger("Idle_State", number);
-                Debug.Log("Shotgun");
+                //Debug.Log(equipped);
                 break;
             default:
                 equipped = Inventory.Knife;
                 anim.SetInteger("Idle_State", 1);
-                Debug.Log("Knife");
+                //Debug.Log(equipped);
                 break;
 
         }
 
     }
+    
+    void OnFire()
+    {
+        Debug.Log("ATTACKED");
+        if(rb.velocity.magnitude < 1f && equipped != Inventory.Knife)
+        {
+            switch (equipped)
+            {
+                case Inventory.Sword:
+                    anim.Play("Sword_Attack"); break;
+                case Inventory.Handgun:
+                    anim.Play("Handgun_Fire"); break;
+                case Inventory.Shotgun:
+                    anim.Play("Shotgun_Fire"); break;
+                default:
+                    break;
+            }
+
+        }
+        else
+        {
+            if(equipped == Inventory.Knife)
+            {
+                Debug.Log("SLASH");
+
+                anim.Play("Slash");
+            }
+            
+        }
+    }
+
+
+
+
 
 
 }
