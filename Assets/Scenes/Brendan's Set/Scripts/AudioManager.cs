ba63cd27ec32bb1388cf03aa09ca9e0e17996a56
NewbornLynx <87749798+NewbornLynx@users.noreply.github.com>
2024-03-12 20:20:40 -0700

    Added Background Music

---
+++ Assets/Scenes/Brendan's Set/Scripts/AudioManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("---------Audio Source---------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("---------Audio Clip---------")]
+    public AudioClip background;
+    public AudioClip death;
+    public AudioClip damage;
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+}
